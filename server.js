82ae88ba6a629ec505a94c225a1b7d995011813e
yansudarshan <sudarshanyanpallewar415@gmail.com>
2026-03-08 11:27:42 +0530

    ipv4

--- server.js
+++ server.js
@@ -278,6 +278,7 @@ cloudinary.config({
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
+  family:4,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
